1cf79c2667b196aa4aa6cedddbd3530c1ab65525
Ashwinviji <123438275+Ashwinviji@users.noreply.github.com>
2023-01-25 11:01:23 +0530

    Add files via upload

---
+++ ash4.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a Number:"))
+def ispalidrome (x):
+    return str(x) == [-1]
+print("the is palidrome or not (true/false):",ispalidrome (a))
---
+++ ash5.py
@@ -0,0 +1,8 @@
+loaves = int(input("Enter number of loaves of day old bread:"))
+regular_price = loaves * 185
+discount = regular_price * 0.6
+total_price = regular_price - discount
+
+print("regular price:{:2f}rupees".format(regular_price))
+print("discount:{:2f}rupees".format(discount))
+print("total price:{:2f}rupees".format(total_price))
